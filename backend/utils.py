881f349093b66026eb5ff1d73ab4a517df32e967
Markus Reinhold <markus@leaffan.net>
2019-03-07 23:44:32 +0100

    Add utility function to retrieve season from game date

--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import date
+
 name_corrections = {
     # Coaches
     'Dahlem Fabian': 'Fabian Dahlem',
@@ -43,3 +45,19 @@ def get_team_from_game(game, home_road):
         return game['road_abbr']
     else:
         return None
+
+
+def get_season(date_of_interest=None):
+    """
+    Identifies season based on month of given date, anything until June
+    belongs to the season ending in the date's year, anything after
+    June belongs to the season beginning in the date's year.
+    """
+    if date_of_interest is None:
+        date_of_interest = date.today()
+    if date_of_interest.month < 7:
+        season = date_of_interest.year - 1
+    else:
+        season = date_of_interest.year
+
+    return season
