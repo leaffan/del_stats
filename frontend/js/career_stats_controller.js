4def8f996d8afc4d683df0a4c44dc3530d1fe07b
Markus Reinhold <markus@leaffan.net>
2019-10-05 04:32:30 +0200

    Extend functionality on career stats page

--- frontend/js/career_stats_controller.js
+++ frontend/js/career_stats_controller.js
@@ -1,12 +1,16 @@
 app.controller('careerStatsController', function ($scope, $http, $routeParams, svc) {
-
     $scope.svc = svc;
     $scope.table_select = 'career_stats_skaters';
+    $scope.season_type = 'ALL';
     $scope.sortConfig = {
         'sortKey': 'pts',
         'sortCriteria': ['pts', 'ptspg', 'g', '-gp'],
         'sortDescending': true
     }
+    $scope.sort_def = {
+        "pts": ['pts', 'ptspg', 'g', '-gp'],
+        "ptspg": ['ptspg', '-gp', 'sog'],
+    };
 
     // retrieving column headers (and abbreviations + explanations)
     $http.get('./js/career_stats_columns.json').then(function (res) {
@@ -16,19 +20,99 @@ app.controller('careerStatsController', function ($scope, $http, $routeParams, s
     // loading stats from external json file
     $http.get('data/career_stats/career_stats.json').then(function (res) {
         $scope.player_stats = res.data;
-        $scope.full_player_stats = [];
+        var all_seasons = new Set();
+        var all_teams = new Set()
         $scope.player_stats.forEach(element => {
-            if (element['career']['all']) {
-                c_stats = element['career']['all'];
-                c_stats['player_id'] = element['player_id'];
-                c_stats['first_name'] = element['first_name'];
-                c_stats['last_name'] = element['last_name'];
-                c_stats['position'] = element['position'];
-                $scope.full_player_stats.push(c_stats);
-            }
+            element['seasons'].forEach(season_stat_line => {
+                all_seasons.add(season_stat_line['season']);
+                all_teams.add(season_stat_line['team']);
+            })
         });
-
+        $scope.min_season = Math.min(...all_seasons);
+        $scope.max_season = Math.max(...all_seasons);
+        $scope.from_season = $scope.min_season;
+        $scope.to_season = $scope.max_season;
+        $scope.all_teams = [...all_teams];
     });
+
+    $scope.$watchGroup(['from_season', 'to_season', 'season_type', 'team', 'position'], function() {
+        if ($scope.player_stats) {
+            $scope.filtered_season_player_stats = $scope.filterCareerStats();
+        }
+    }, true);
+
+    $scope.to_aggregate = ['gp', 'g', 'a', 'pts', 'plus_minus', 'pim', 'ppg', 'shg', 'gwg', 'sog']
+
+    $scope.filterCareerStats = function() {
+        filtered_career_stats = [];
+        if ($scope.player_stats === undefined)
+            return filtered_career_stats;
+        $scope.player_stats.forEach(player => {
+            // setting up filtered cumulated stat line for current player
+            filtered_stat_line = {
+                'player_id': player['player_id'],
+                'first_name': player['first_name'],
+                'last_name': player['last_name'],
+                'position': player['position'],
+                'sh_pctg': 0.0,
+                'gpg': 0.0,
+                'apg': 0.0,
+                'ptspg': 0.0
+            };
+            $scope.to_aggregate.forEach(category => {
+                filtered_stat_line[category] = 0;
+            })
+            player['seasons'].forEach(season_stat_line => {
+                in_season_range = false;
+                in_season_types = false;
+                is_selected_team = false;
+                is_selected_position = false;
+
+                // checking if current stat line is from a selected season
+                if (season_stat_line['season'] >= $scope.from_season && season_stat_line['season'] <= $scope.to_season)
+                    in_season_range = true;
+                // checking if current stat line is of a selected season type
+                if ($scope.season_type == 'ALL') {
+                    in_season_types = true;
+                } else {
+                    if ($scope.season_type == season_stat_line['season_type'])
+                        in_season_types = true;
+                };
+                // checking if current stat line is for the selected team
+                if (!$scope.team) {
+                    is_selected_team = true;
+                } else {
+                    if (season_stat_line['team'] == $scope.team)
+                        is_selected_team = true;
+                };
+                // checking if current stat line is by a selected position
+                // TODO: move outside of this loop
+                if (!$scope.position) {
+                    is_selected_position = true;
+                } else {
+                    if (player['position'] == $scope.position)
+                        is_selected_position = true;
+                }
+                // finally aggregating values of all season stat lines that have been filtered
+                if (in_season_range && in_season_types && is_selected_team && is_selected_position) {
+                    $scope.to_aggregate.forEach(category => {
+                        filtered_stat_line[category] += season_stat_line[category];
+                    });
+                }
+            });
+            // calculating shooting percentage
+            if (filtered_stat_line['sog'])
+                filtered_stat_line['sh_pctg'] = filtered_stat_line['g'] / filtered_stat_line['sog'] * 100.
+            // calculating per-game statistics
+            if (filtered_stat_line['gp']) {
+                filtered_stat_line['gpg'] = filtered_stat_line['g'] / filtered_stat_line['gp'];
+                filtered_stat_line['apg'] = filtered_stat_line['a'] / filtered_stat_line['gp'];
+                filtered_stat_line['ptspg'] = filtered_stat_line['pts'] / filtered_stat_line['gp'];
+            };
+            filtered_career_stats.push(filtered_stat_line);
+        });
+        return filtered_career_stats;
+    };
 
     $scope.hasCareerFilter = function(a) {
         if (!a['career']['all']) {
@@ -37,10 +121,7 @@ app.controller('careerStatsController', function ($scope, $http, $routeParams, s
         return true;
     };
 
-    $scope.sort_def = {
-        "pts": ['pts', 'ptspg', 'g', '-gp']
-    };
-
+    // TODO: replace with external definition
     $scope.setSortOrder2 =  function(sortKey, oldSortConfig) {
         ascendingAttrs = ['full_name'];
         // if previous sort key equals the new one
@@ -69,6 +150,12 @@ app.controller('careerStatsController', function ($scope, $http, $routeParams, s
                     'sortDescending': true
                 }
             }
+        }
+    }
+
+    $scope.greaterThanFilter = function (prop, val) {
+        return function (item) {
+            return item[prop] > val;
         }
     }
 
