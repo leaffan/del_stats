cb4c18cf3b391bdd48034c8689b07f824d9018d7
Markus Reinhold <markus@leaffan.net>
2019-10-16 12:34:36 +0200

    Extend update of head-to-head records

--- backend/update_h2h_records.py
+++ backend/update_h2h_records.py
@@ -32,35 +32,94 @@ if __name__ == '__main__':
     for team_game in team_games[-1]:
         team = team_game['team']
         opp_team = team_game['opp_team']
-        h2h_records[team][opp_team] = defaultdict(int)
-        h2h_records[team][opp_team]['gp'] += team_game['games_played']
-        h2h_records[team][opp_team]['w'] += team_game['w']
-        h2h_records[team][opp_team]['l'] += team_game['l']
+        games_played = team_game['games_played']
+        # setting up new record item for current season if necessary
+        if opp_team not in h2h_records[team]:
+            h2h_records[team][opp_team] = defaultdict(int)
+        curr_record = h2h_records[team][opp_team]
+        curr_record['games_played'] += games_played
+        curr_record['wins'] += team_game['w']
+        curr_record['losses'] += team_game['l']
+        curr_record['goals_for'] += team_game['score']
+        curr_record['goals_against'] += team_game['opp_score']
+        if team_game['game_type'] == 'OT':
+            curr_record['ot_games_played'] += games_played
+        if team_game['game_type'] == 'SO':
+            curr_record['so_games_played'] += games_played
+            curr_record['so_wins'] += team_game['w']
+            curr_record['so_losses'] += team_game['l']
+            curr_record['ot_ties'] += 1
+        elif team_game['game_type'] == 'OT':
+            curr_record['ot_wins'] += team_game['w']
+            curr_record['ot_losses'] += team_game['l']
+        if team_game['season_type'] == 'PO':
+            curr_record['po_games_played'] += games_played
+            curr_record['po_wins'] += team_game['w']
+            curr_record['po_losses'] += team_game['l']
+        if team_game['game_type'] == 'OT' and team_game['season_type'] == 'PO':
+            curr_record['po_ot_games_played'] += games_played
+            curr_record['po_ot_wins'] += team_game['w']
+            curr_record['po_ot_losses'] += team_game['l']
         home_road = team_game['home_road']
-        h2h_records[team][opp_team]["gp_%s" % home_road] += team_game[
-            'games_played']
-        h2h_records[team][opp_team]["w_%s" % home_road] += team_game['w']
-        h2h_records[team][opp_team]["l_%s" % home_road] += team_game['l']
+        curr_record["%s_games_played" % home_road] += games_played
+        curr_record["%s_wins" % home_road] += team_game['w']
+        curr_record["%s_losses" % home_road] += team_game['l']
+        curr_record['%s_goals_for' % home_road] += team_game['score']
+        curr_record['%s_goals_against' % home_road] += team_game['opp_score']
+        if team_game['game_type'] == 'OT':
+            curr_record["%s_ot_games_played" % home_road] += games_played
+            curr_record["%s_ot_wins" % home_road] += team_game['w']
+            curr_record["%s_ot_losses" % home_road] += team_game['l']
+        if team_game['game_type'] == 'SO':
+            curr_record["%s_so_games_played" % home_road] += games_played
+            curr_record["%s_so_wins" % home_road] += team_game['w']
+            curr_record["%s_so_losses" % home_road] += team_game['l']
+        if team_game['season_type'] == 'PO':
+            curr_record["%s_po_games_played" % home_road] += games_played
+            curr_record["%s_po_wins" % home_road] += team_game['w']
+            curr_record["%s_po_losses" % home_road] += team_game['l']
+        if team_game['game_type'] == 'OT' and team_game['season_type'] == 'PO':
+            curr_record["%s_po_ot_games_played" % home_road] += games_played
+            curr_record["%s_po_ot_wins" % home_road] += team_game['w']
+            curr_record["%s_po_ot_losses" % home_road] += team_game['l']
 
     for team in h2h_all_time:
         for opp_team in h2h_all_time[team]:
             if opp_team in h2h_records[team]:
                 for key in [
-                    'gp', 'w', 'l',
-                    'gp_home', 'w_home', 'l_home',
-                    'gp_road', 'w_road', 'l_road'
+                    'games_played', 'wins', 'losses',
+                    'goals_for', 'goals_against',
+                    'ot_games_played', 'ot_wins', 'ot_losses', 'ot_ties',
+                    'so_games_played', 'so_wins', 'so_losses',
+                    'po_games_played', 'po_wins', 'po_losses',
+                    'home_games_played', 'home_wins', 'home_losses',
+                    'home_goals_for', 'home_goals_against',
+                    'home_ot_games_played', 'home_ot_wins', 'home_ot_losses',
+                    'home_so_games_played', 'home_so_wins', 'home_so_losses',
+                    'home_po_games_played', 'home_po_wins', 'home_po_losses',
+                    'home_po_ot_games_played', 'home_po_ot_wins',
+                    'home_po_ot_losses',
+                    'road_games_played', 'road_wins', 'road_losses',
+                    'road_goals_for', 'road_goals_against',
+                    'road_ot_games_played', 'road_ot_wins', 'road_ot_losses',
+                    'road_so_games_played', 'road_so_wins', 'road_so_losses',
+                    'road_po_games_played', 'road_po_wins', 'road_po_losses',
+                    'road_po_ot_games_played', 'road_po_ot_wins',
+                    'road_po_ot_losses',
                 ]:
                     h2h_all_time[team][opp_team][key] += (
                         h2h_records[team][opp_team][key])
-                    h2h_all_time[team][opp_team]['win_pctg'] = round(
-                        h2h_all_time[team][opp_team]['w'] /
-                        h2h_all_time[team][opp_team]['gp'] * 100, 2)
-                    h2h_all_time[team][opp_team]['win_pctg_home'] = round(
-                        h2h_all_time[team][opp_team]['w_home'] /
-                        h2h_all_time[team][opp_team]['gp_home'] * 100, 2)
-                    h2h_all_time[team][opp_team]['win_pctg_road'] = round(
-                        h2h_all_time[team][opp_team]['w_road'] /
-                        h2h_all_time[team][opp_team]['gp_road'] * 100, 2)
+                h2h_all_time[team][opp_team]['win_pctg'] = round(
+                    h2h_all_time[team][opp_team]['wins'] /
+                    h2h_all_time[team][opp_team]['games_played'] * 100, 2)
+                h2h_all_time[team][opp_team]['home_win_pctg'] = round(
+                    h2h_all_time[team][opp_team]['home_wins'] /
+                    h2h_all_time[team][opp_team]['home_games_played'] *
+                    100, 2)
+                h2h_all_time[team][opp_team]['road_win_pctg'] = round(
+                    h2h_all_time[team][opp_team]['road_wins'] /
+                    h2h_all_time[team][opp_team]['road_games_played'] *
+                    100, 2)
 
     tgt_h2h_path = os.path.join(tgt_dir, SEASON_HEAD2HEAD_TGT)
     open(tgt_h2h_path, 'w').write(json.dumps(h2h_all_time, indent=2))
