dff55b68c248df222139ce811d3b9ea3db849e18
Markus Reinhold <markus@leaffan.net>
2019-10-23 18:35:25 +0200

    Declare date format for timespan change explicitly

--- frontend/js/player_profile_controller.js
+++ frontend/js/player_profile_controller.js
@@ -192,8 +192,8 @@ app.controller('plrProfileController', function($scope, $http, $routeParams, $lo
         } else {
             season = parseInt($scope.season);
         }
-        ctrl.fromDate = moment(season + '-' + timespanSelect + '-1');
-        ctrl.toDate = moment(season + '-' + timespanSelect + '-1').endOf('month');
+        ctrl.fromDate = moment(season + '-' + timespanSelect + '-1', 'YYYY-M-D');
+        ctrl.toDate = moment(season + '-' + timespanSelect + '-1', 'YYYY-M-D').endOf('month');
     }
 
 });
