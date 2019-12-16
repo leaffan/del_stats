27db9db7084dfcd1badffe20bf349c48ecc4e5f5
Markus Reinhold <markus@leaffan.net>
2019-12-16 13:55:28 +0100

    Include additional copy targets

--- backend/_copy_del_stats.sh
+++ backend/_copy_del_stats.sh
@@ -6,6 +6,7 @@ season="${1:-2019}"
 cp -v /home/pacs/mre00/users/markus/_del/_data/del_players.json /home/doms/leaffan.net/subs/www/del/data/del_players.json
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_goalie_game_stats.json /home/doms/leaffan.net/subs/www/del/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_player_game_stats_aggregated.* /home/doms/leaffan.net/subs/www/del/data/$season
+cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_player_game_stats.csv /home/doms/leaffan.net/subs/www/del/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_team_game_stats.json /home/doms/leaffan.net/subs/www/del/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_streaks_loose.json /home/doms/leaffan.net/subs/www/del/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_streaks_strict.json /home/doms/leaffan.net/subs/www/del/data/$season
@@ -20,10 +21,12 @@ cp -TRv /home/pacs/mre00/users/markus/_del/_data/career_stats/per_team/ /home/do
 # copying data sets to extended site location
 cp -v /home/pacs/mre00/users/markus/_del/_data/del_players.json /home/doms/leaffan.net/subs/www/del_extended/data/del_players.json
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/coaches.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
+cp -v /home/pacs/mre00/users/markus/_del/_data/$season/h2h.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/full_schedule.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_goalie_game_stats.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_goalie_game_stats_aggregated.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_player_game_stats_aggregated.* /home/doms/leaffan.net/subs/www/del_extended/data/$season
+cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_player_game_stats.csv /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_team_game_stats.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_streaks_loose.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
 cp -v /home/pacs/mre00/users/markus/_del/_data/$season/del_streaks_strict.json /home/doms/leaffan.net/subs/www/del_extended/data/$season
