de820fc254e4f8fd7b19c3dd00fa7da10bd0b5c5
Markus Reinhold <markus@leaffan.net>
2020-01-09 14:58:50 +0100

    Fix calculation of days left until next fixtures

--- frontend/js/previews_controller.js
+++ frontend/js/previews_controller.js
@@ -5,12 +5,15 @@ app.controller('previewsController', function($scope, $http, $localStorage, $rou
     $scope.next_round = "0";
     $scope.min_days_left = 365;
     // loading schedule from external json file
+    var now = moment();
     $http.get('data/'+ $scope.season + '/full_schedule.json').then(function (res) {
         $scope.fixtures = res.data;
         $scope.fixtures.forEach(fixture => {
             fixture['game_date'] = moment(fixture['start_date']);
+            // retrieving start of day for game date
+            fixture['game_day'] = moment(fixture['start_date']).startOf('day');
             // calculating days left until current fixture
-            fixture['days_left'] = fixture['game_date'].diff(moment(), 'days');
+            fixture['days_left'] = fixture['game_day'].diff(now, 'days');
             if (fixture['days_left'] >= 0 && fixture['days_left'] < $scope.min_days_left) {
                 $scope.next_round = fixture['round'];
                 $scope.min_days_left = fixture['days_left'];
