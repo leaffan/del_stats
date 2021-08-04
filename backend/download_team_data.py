c0e4624d562108cdcda3d1680b5ca87f2b45cdd4
Markus Reinhold <markus@leaffan.net>
2021-08-04 12:02:10 +0200

    Use team, game type and season choices from configuration; fix code layout

--- backend/download_team_data.py
+++ backend/download_team_data.py
@@ -9,8 +9,13 @@ import yaml
 import requests
 import argparse
 
+# loading external configuration
+CONFIG = yaml.safe_load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.yml')))
 
 def get_download_targets(args, config):
+    """
+    Retrieves seasons, game types and teams to download data for.
+    """
     # setting target team(s)
     tgt_team = args.team
     if not tgt_team:
@@ -28,10 +33,7 @@ def get_download_targets(args, config):
     if not tgt_game_type or tgt_game_type == 'ALL':
         game_types = list(config['game_types'].keys())
     else:
-        game_types = {
-            k: v for (k, v) in config['game_types'].items() if
-            v == tgt_game_type
-        }
+        game_types = {k: v for (k, v) in config['game_types'].items() if v == tgt_game_type}
 
     return seasons, game_types, teams
 
@@ -39,43 +41,34 @@ def get_download_targets(args, config):
 if __name__ == '__main__':
 
     # retrieving arguments specified on command line
-    parser = argparse.ArgumentParser(
-        description='Download DEL team information.')
+    parser = argparse.ArgumentParser(description='Download DEL team information.')
     parser.add_argument(
-        '-t', '--team', dest='team', required=False,
-        metavar='team to download data for',
-        choices=[
-            'ING', 'MAN', 'EBB', 'DEG', 'KEV', 'STR', 'IEC',
-            'WOB', 'BHV', 'KEC', 'RBM', 'AEV', 'NIT', 'SWW'],
+        '-t', '--team', dest='team', required=False, metavar='team to download data for',
+        choices=list(CONFIG['teams'].values()),
         help="The team for which information will be downloaded for")
     parser.add_argument(
-        '-s', '--season', dest='season', required=False, type=int,
-        metavar='season to download data for',
-        choices=[2016, 2017, 2018, 2019, 2020],
+        '-s', '--season', dest='season', required=False, type=int, metavar='season to download data for',
+        default=CONFIG['default_season'], choices=CONFIG['seasons'],
         help="The season for which information will be downloaded for")
     parser.add_argument(
-        '-g', '--game_type', dest='game_type', required=False,
-        metavar='game type to download data for', choices=['RS', 'PO', 'MSC', 'ALL'],
+        '-g', '--game_type', dest='game_type', required=False, metavar='game type to download data for',
+        choices=list(CONFIG['game_types'].values()) + ['ALL'],
         help="The game type for which information will be downloaded for")
     parser.add_argument(
         'category', metavar='information category',
-        help='information category to be downloaded',
-        choices=['schedules', 'team_stats', 'roster_stats'])
-
-    # loading external configuration
-    config = yaml.safe_load(open(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), 'config.yml')))
+        choices=['schedules', 'team_stats', 'roster_stats'],
+        help='information category to be downloaded')
 
     args = parser.parse_args()
-    seasons, game_types, teams = get_download_targets(args, config)
+    seasons, game_types, teams = get_download_targets(args, CONFIG)
     print("+ Downloading %s data" % args.category)
-    print("+ Using base url %s" % config['base_url'])
+    print("+ Using base url %s" % CONFIG['base_url'])
 
     # retrieving configuration
-    base_url = config['base_url']
-    tgt_base_dir = config['tgt_base_dir']
+    base_url = CONFIG['base_url']
+    tgt_base_dir = CONFIG['tgt_base_dir']
     tgt_sub_dir = args.category
-    target_url_component = config['url_components'][args.category]
+    tgt_url_component = CONFIG['url_components'][args.category]
 
     # retrieving or setting up dictionary with dates of last modification
     last_modified_path = os.path.join(tgt_base_dir, 'last_modified.json')
@@ -85,36 +78,29 @@ if __name__ == '__main__':
         last_modified_dict = dict()
 
     for season in seasons:
-        print(
-            "+ Downloading %s for %d-%d" % (args.category, season, season + 1))
+        print("+ Downloading %s for %d-%d" % (args.category, season, season + 1))
         for team_id in teams:
             sys.stdout.write("%s:" % teams[team_id])
             sys.stdout.flush()
             for game_type in game_types:
                 # setting up target url
-                target_url = R"/".join((
-                    base_url, target_url_component,
-                    str(season), str(game_type), "%d.json" % team_id))
+                tgt_url = R"/".join((base_url, tgt_url_component, str(season), str(game_type), "%d.json" % team_id))
 
                 # setting up target directory and path
-                tgt_dir = os.path.join(
-                    tgt_base_dir, tgt_sub_dir, str(season), str(game_type))
+                tgt_dir = os.path.join(tgt_base_dir, tgt_sub_dir, str(season), str(game_type))
                 if not os.path.isdir(tgt_dir):
                     os.makedirs(tgt_dir)
                 tgt_path = os.path.join(tgt_dir, "%d.json" % team_id)
 
                 # setting up customized header
                 req_header = dict()
-                if (
-                    os.path.isfile(tgt_path) and
-                    target_url in last_modified_dict
-                ):
-                    req_header['If-Modified-Since'] = last_modified_dict[
-                        target_url]
+                if os.path.isfile(tgt_path) and tgt_url in last_modified_dict:
+                    req_header['If-Modified-Since'] = last_modified_dict[tgt_url]
 
                 # retrieving target data using customized header
                 try:
-                    r = requests.get(target_url, headers=req_header)
+                    r = requests.get(tgt_url, headers=req_header)
+                    # downloading new or modified data
                     if r.status_code == 200:
                         data = r.json()
                         sys.stdout.write('+')
@@ -124,28 +110,28 @@ if __name__ == '__main__':
                         sys.stdout.write('.')
                         sys.stdout.flush()
                         continue
-                    # data not available, i.e. playoff stats for non-playoff
-                    # teams
+                    # data not available, i.e. playoff stats for non-playoff teams
                     elif r.status_code == 404:
                         sys.stdout.write('X')
                         sys.stdout.flush()
                         continue
                 except json.decoder.JSONDecodeError:
-                    print(
-                        "Unable to retrieve JSON data from %s" % target_url)
+                    print("Unable to retrieve JSON data from %s" % tgt_url)
                     continue
 
+                # sorting roster stats by player id in order to allow for data versioning
                 if args.category == 'roster_stats':
                     data = sorted(data, key=lambda k: k['id'])
 
+                # writing downloaded data
                 open(tgt_path, 'w').write(json.dumps(data, indent=2))
 
                 # retrieving date of last modification
                 last_modified = r.headers['Last-Modified']
-                last_modified_dict[target_url] = last_modified
+                last_modified_dict[tgt_url] = last_modified
                 time.sleep(0.1)
             sys.stdout.write(' ')
         print()
 
-    open(last_modified_path, 'w').write(
-        json.dumps(last_modified_dict, indent=2))
+    # re-writing dictionary with timestamp of last modification of source files
+    open(last_modified_path, 'w').write(json.dumps(last_modified_dict, indent=2))
